2a8c945b62b39dc4cccef4bf5443201dd66f1b52
patrickp246@gmail.com <patrickp246@gmail.com>
2018-08-08 10:40:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function theBeatlesPlay(musicians, instruments) {
+  var list = Array();
+  
+  for(var i = 0; i < musicians.length; i++) {
+    // list.push(musicians[i] + " plays " + instruments[i]);
+    list.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  
+  return list;
+}
 
+function johnLennonFacts(facts) {
+  var i = 0;
+  while(i < facts.length) {
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var list = [];
+  
+  do {
+    
+  }
+  while()
+}
